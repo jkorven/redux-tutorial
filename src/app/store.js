a18acd84cf1979d41f31711c6fa150c536b0898a
jkorven <finalityreborn@gmail.com>
2023-03-26 22:18:26 -0500

    new

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import postsReducers from '../features/posts/postsSlice'
 
 export const store = configureStore({
     reducer: {
-        
+        posts: postsReducers
     }
 })
